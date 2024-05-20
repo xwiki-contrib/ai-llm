e00f6b85c88d4e843382cad91af9e4b6927bebd5
Paul Pantiru <paul.pantiru@xwiki.com>
2024-05-20 23:22:32 +0300

    LLMAI-54: Develop a chat UI that is independent of XWiki and can be embedded in any application * Move base url definition to data-base-url

--- application-ai-llm-chat/application-ai-llm-chat-webjar/webjar/chatUILoader.js
+++ application-ai-llm-chat/application-ai-llm-chat-webjar/webjar/chatUILoader.js
@@ -4,6 +4,13 @@ window.onload = () => {
     const fab = document.getElementById('fab');
     const pane = document.getElementById('pane');
     
+    // Retrieve the base URL from the script tag
+    const scriptTag = document.getElementById('chat-loader');
+    const baseURL = scriptTag.getAttribute('data-base-url');
+    
+    // Set the base URL in XWikiAiAPI
+    XWikiAiAPI.setBaseURL(baseURL);
+
     XWikiAiAPI.setChatUISettings(["temperature","model","stream"]); //"server-address","temperature","model","stream"
   
     // Chat pane buttons handlers
@@ -21,14 +28,14 @@ window.onload = () => {
         0.5, // temperature
         [], // messages
         true, // streaming
-    )
+    );
   
     setTimeout(async () => {
         const chatSettings = document.getElementById('chat-settings');
         if (chatSettings && typeof chatSettings.getSettings === "function") {
             try {
                 const settings = await chatSettings.getSettings();
-                if (!settings.llmServerAddress){
+                if (!settings.llmServerAddress) {
                     settings.llmServerAddress = XWikiAiAPI.getBaseURL();
                 }
                 XWikiAiAPI.setBaseURL(settings.llmServerAddress);
@@ -44,81 +51,81 @@ window.onload = () => {
     }, 500);
   
     async function sendMessageToLLM(completionRequest) {
-      let incomingMessage = await pane.addIncomingMessage("");
-      // Create separate container for loading animation and text content
-      incomingMessage.innerHTML = '<div class="message-text"><div class="waiting-line"><div class="dot dot1"></div><div class="dot dot2"></div><div class="dot dot3"></div></div></div>';
+        let incomingMessage = await pane.addIncomingMessage("");
+        // Create separate container for loading animation and text content
+        incomingMessage.innerHTML = '<div class="message-text"><div class="waiting-line"><div class="dot dot1"></div><div class="dot dot2"></div><div class="dot dot3"></div></div></div>';
   
-      function removeLoadingAnimation() {
-          const loadingSpinner = incomingMessage.querySelector('.waiting-line');
-          if (loadingSpinner) {
-              loadingSpinner.remove(); // Remove only the loading spinner
-          }
-      }
+        function removeLoadingAnimation() {
+            const loadingSpinner = incomingMessage.querySelector('.waiting-line');
+            if (loadingSpinner) {
+                loadingSpinner.remove(); // Remove only the loading spinner
+            }
+        }
     
-      if (completionRequest.stream) {
-          let messageText = '';
-          XWikiAiAPI.getCompletions(completionRequest, async (messageChunk) => {
-              const messageTextContainer = incomingMessage.querySelector('.message-text');
-              messageText += messageChunk.choices[0].delta.content;
-              messageTextContainer.innerHTML = marked.parse(messageText);
-          })
-          .then(() => {
-              completionRequest.addMessage("assistant", incomingMessage.querySelector('.message-text').textContent);
-              return completionRequest;
-          })
-          .catch(err => {
-              console.error(err);
-          })
-      } else {
-          return XWikiAiAPI.getCompletions(completionRequest).then(async messageChunk => {
-              removeLoadingAnimation(); // Clear loading before showing the message
-              incomingMessage.querySelector('.message-text').innerHTML = marked.parse(messageChunk.choices[0].message.content);
-              completionRequest.addMessage("assistant", incomingMessage.querySelector('.message-text').textContent);
-          }).catch(err => {
-              console.error(err);
-              removeLoadingAnimation(); // Ensure to clear loading spinner in case of error
-          }); 
-      }
-  }
+        if (completionRequest.stream) {
+            let messageText = '';
+            XWikiAiAPI.getCompletions(completionRequest, async (messageChunk) => {
+                const messageTextContainer = incomingMessage.querySelector('.message-text');
+                messageText += messageChunk.choices[0].delta.content;
+                messageTextContainer.innerHTML = marked.parse(messageText);
+            })
+            .then(() => {
+                completionRequest.addMessage("assistant", incomingMessage.querySelector('.message-text').textContent);
+                return completionRequest;
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        } else {
+            return XWikiAiAPI.getCompletions(completionRequest).then(async messageChunk => {
+                removeLoadingAnimation(); // Clear loading before showing the message
+                incomingMessage.querySelector('.message-text').innerHTML = marked.parse(messageChunk.choices[0].message.content);
+                completionRequest.addMessage("assistant", incomingMessage.querySelector('.message-text').textContent);
+            }).catch(err => {
+                console.error(err);
+                removeLoadingAnimation(); // Ensure to clear loading spinner in case of error
+            }); 
+        }
+    }
   
     const wait = () => new Promise(resolve => setTimeout(resolve, 500));
   
     function handleIncomingMessage(event) {
-      let message = event.detail.element;
-      wait()
+        let message = event.detail.element;
+        wait()
         .then(() => message.state = 'delivered')
         .then(() => completionRequest.addMessage("user", event.detail.text))
         .then(() => message.state = 'read')
         .then(() => sendMessageToLLM(completionRequest));
     }
     pane.addEventListener('incoming', handleIncomingMessage);    
-}
+};
 
-function insertChatUI(){
-const htmlContent = `
-<fab-app id="fab">
-    <chat-pane id="pane">
-        <ion-toolbar slot="header" color="primary">
-        <ion-title>XWiki AI Chat</ion-title>
-        <ion-buttons slot="primary">
-            <!-- settings button -->
-            <ion-button id="chat-settings-btn">
-                <ion-icon slot="icon-only" name="settings" />
-            </ion-button>
-            <!-- close button -->
-            <ion-button id="chat-close-btn">
-                <ion-icon slot="icon-only" name="close" />
-            </ion-button>
-        </ion-buttons>
-        </ion-toolbar>
-        <chat-settings id="chat-settings" hidden></chat-settings>
-        <ion-card style="background: white;">
-        <ion-card-content>
-        <p>Chat with <i>WAISE-Bot</i>!</p>
-        </ion-card-content>
-        </ion-card>
-    </chat-pane>
-</fab-app>
-`;
-document.body.innerHTML += htmlContent;
+function insertChatUI() {
+    const htmlContent = `
+        <fab-app id="fab">
+            <chat-pane id="pane">
+                <ion-toolbar slot="header" color="primary">
+                <ion-title>XWiki AI Chat</ion-title>
+                <ion-buttons slot="primary">
+                    <!-- settings button -->
+                    <ion-button id="chat-settings-btn">
+                        <ion-icon slot="icon-only" name="settings" />
+                    </ion-button>
+                    <!-- close button -->
+                    <ion-button id="chat-close-btn">
+                        <ion-icon slot="icon-only" name="close" />
+                    </ion-button>
+                </ion-buttons>
+                </ion-toolbar>
+                <chat-settings id="chat-settings" hidden></chat-settings>
+                <ion-card style="background: white;">
+                <ion-card-content>
+                <p>Chat with <i>WAISE-Bot</i>!</p>
+                </ion-card-content>
+                </ion-card>
+            </chat-pane>
+        </fab-app>
+    `;
+    document.body.innerHTML += htmlContent;
 }
